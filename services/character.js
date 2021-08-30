0cd7c525ea56fa5856b78a5a798057ed7111ffdc
abibflores <abib.m@reworth.co>
2021-08-30 13:55:39 -0500

    feat(API): get characters with axios

---
+++ services/character.js
@@ -0,0 +1,18 @@
+// https://rickandmortyapi.com/api/character
+import axios from "axios";
+
+const getCharacters = async () => {
+    try {
+        const resp = await axios.get(
+            "https://rickandmortyapi.com/api/character",
+        );
+
+        if (resp.status === 200) {
+            return { ...resp.data.results };
+        }
+    } catch (err) {
+        console.error("error: ", err);
+    }
+};
+
+export default getCharacters;
